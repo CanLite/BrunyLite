9839754314cb61b104637a3a786a140faaa91402
CanLite <canlite24@outlook.com>
2025-05-09 18:19:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 6457;
 
 app.use(cors());
 app.use(express.json());
