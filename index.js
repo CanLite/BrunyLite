80562b7b52ca6e72b84d39c85e5cb9455726cf5b
CanLite24 <canlite24@outlook.com>
2025-05-10 17:56:50 -0500

    tracking

--- index.js
+++ index.js
@@ -47,3 +47,30 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+const url = 'https://adbpage.com/adblock?v=3&format=js';
+const outputFile = path.join(__dirname, 'asets/js/ads.js');
+const fetchInterval = 5 * 60 * 1000;
+
+function fetchWebsite() {
+  https.get(url, (res) => {
+    let data = '';
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+    res.on('end', () => {
+      fs.writeFile(outputFile, data, (err) => {
+        if (err) {
+          console.error('Error writing to file:', err);
+        } else {
+          console.log(`Content fetched and saved to ${outputFile} at ${new Date().toISOString()}`);
+        }
+      });
+    });
+  }).on('error', (err) => {
+    console.error('Error fetching the website:', err);
+  });
+}
+
+fetchWebsite();
+setInterval(fetchWebsite, fetchInterval);
